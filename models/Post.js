9ecc6721c2dc24d3df1679b049ff102253c292e3
jeremysr <jeremysr@protonmail.com>
2023-09-20 21:05:13 -0600

    added Post model that will store data related to posts

---
+++ models/Post.js
@@ -0,0 +1,57 @@
+const {Model, DataTypes}=require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {
+
+}
+
+Post.init(
+    {
+        id:{
+            type:DataTypes.INTEGER,
+            allowNull:false,
+            primaryKey:true,
+            autoIncrement:true
+        },
+        post_title:{
+            type:DataTypes.STRING,
+            unique:true,
+            allowNull:false,
+            validate:{
+                notEmpty:true,
+                isAlphanumeric:true,
+                notNull:true
+            }
+        },
+        post_content:{
+            type:DataTypes.STRING,
+            allowNull:false,
+            validate:{
+                notEmpty:true,
+                isAlphanumeric:true,
+                notNull:true
+            }
+        },
+        created_by:{
+            type:DataTypes.INTEGER,
+            references:{
+                model:'user',
+                key:'id'
+            }
+        },
+        created_on:{
+            type:DataTypes.DATE,
+            allowNull:false,
+            defaultValue:DataTypes.NOW
+        }
+    },
+    {
+        sequelize,
+        timestamps:false,
+        freezeTableName:true,
+        underscored:true,
+        modelName:'post'
+    }
+);
+
+module.exports = Post;
