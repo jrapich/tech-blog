493f3b7dc856aaee4e4898fdbec0c8a6c95760e6
jeremysr <jeremysr@protonmail.com>
2023-09-20 20:48:21 -0600

    added User model with several validations for email, username, password length, etc

---
+++ models/User.js
@@ -0,0 +1,65 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+      }
+}
+
+User.init(
+    {
+        id:{
+            type:DataTypes.INTEGER,
+            allowNull:false,
+            primaryKey:true,
+            autoIncrement:true
+        },
+        username:{
+            type:DataTypes.STRING,
+            allowNull:false,
+            unique:true,
+            validate:{
+                notEmpty:true,
+                isAlphanumeric: true,
+                notEmpty:true,
+                notNull:true
+            }
+        },
+        email:{
+            type:DataTypes.STRING,
+            allowNull:false,
+            unique:true,
+            validate:{
+                isEmail:true,
+                notEmpty:true,
+                notNull:true
+            }
+        },
+        password:{
+            type:DataTypes.STRING,
+            allowNull:false,
+            validate:{
+                notEmpty:true,
+                min:[8],
+                notNull:true
+            }
+        }
+    },
+    {
+        hooks:{
+            beforeCreate: async (newUserData)=>{
+                newUserData.password= await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            }
+        },
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+);
+
+module.exports = User;
