e262559c0f9276f09721806905dd6c880ee6b1d6
jeremysr <jeremysr@protonmail.com>
2023-09-24 12:06:10 -0600

    added basic route to display all post on the home page

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
+const {Post} = require('../models');
 
 router.get('/', async (req, res) => {
-    res.render('homepage');
+
+    try {
+        //get a list of all posts and show the 5 most recent
+        //only show the post titles, no comments as we aren't checking if the user is logged in yet
+        const allPosts = await Post.findAll();
+        let posts;
+        (allPosts.length > 5) ? posts = allPosts.slice(-5) : posts = allPosts;
+        res.render('homepage', posts);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+
 });
 
 module.exports = router;
