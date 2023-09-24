637c34680fb1c3a88131b09d72cae33a6a55a221
Jeremy Rapich <jeremysr@protonmail.com>
2023-09-24 17:47:44 -0600

    added utility that will keep a list of all current users in the db

---
+++ utils/userLIst.js
@@ -0,0 +1,13 @@
+const {User} = require('../models');
+
+const getUserList = async () =>{
+    const userData = await User.findAll();
+    const userListData = userData.map((users) => users.get({ plain: true }));
+    let userList = [];
+    for (let i = 0; i < userListData.length; i++) {
+        userList.push(userListData[i].username);
+    }
+    return userList;
+}
+
+module.exports = getUserList;
