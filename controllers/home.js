da7590ed7faab25bbe1c4da62547eff2837326d0
jeremysr <jeremysr@protonmail.com>
2023-09-24 12:12:04 -0600

    added route to display the login page

--- controllers/home.js
+++ controllers/home.js
@@ -17,4 +17,15 @@ router.get('/', async (req, res) => {
 
 });
 
+router.get('/login', async (req, res) => {
+    try {
+        res.render('login');
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+
+
+
 module.exports = router;
