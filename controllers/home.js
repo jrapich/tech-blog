962bf1387527ba2a5b5f40f6f70ff739620c9aee
jeremysr <jeremysr@protonmail.com>
2023-09-24 12:32:50 -0600

    added route to display a certain post by id

--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
+const { User } = require('../../../../UofU-VIRT-FSF-PT-06-2023-U-LOLC/14-MVC/01-Activities/24-Stu_Auth-Review/Solved/models');
 const {Post} = require('../models');
 const auth = require('../utils/isAuth');
 
 router.get('/', async (req, res) => {
-
     try {
         //get a list of all posts and show the 5 most recent
         //only show the post titles, no comments as we aren't checking if the user is logged in yet
@@ -15,7 +15,6 @@ router.get('/', async (req, res) => {
         console.error(err);
         res.status(500).json(err);
     }
-
 });
 
 router.get('/login', async (req, res) => {
@@ -37,6 +36,16 @@ router.get('/posts', auth, async (req, res) => {
     }
 });
 
-
+router.get('/posts/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findByPk(req.params.id, {
+            include:[{all:true}]
+        });
+        res.render('post', post)
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
