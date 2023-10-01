77538db50a5eb0c1a9fa727ca7ea4913714ea1fe
jeremysr <jeremysr@protonmail.com>
2023-10-01 14:29:45 -0600

    added route for modifying comments

--- controllers/api/modify.js
+++ controllers/api/modify.js
@@ -17,5 +17,20 @@ router.put('/post/:id', async (req, res) => {
     }
 });
 
+router.put('/comment/:id', async (req, res) => {
+    try {
+        const {comment_content} = req.body;
+        const commentUpdate = await Comment.update({
+            comment_content:comment_content
+        }, 
+        {where:{id:req.params.id}
+        });
+        res.json(commentUpdate);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
 
 module.exports = router;
