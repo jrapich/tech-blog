c92b2d1980bc3b2720f8e5225f639e1695df0375
jeremysr <jeremysr@protonmail.com>
2023-10-01 13:16:38 -0600

    fixed front end event listenor from trying to select post id's that aren't on the page

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,3 @@
-const postRows = document.querySelectorAll('.postrow');
-
-
 const postIDs = async () => {
     const response = await fetch('/posts/id/all');
     const posts = await response.json();
@@ -9,13 +6,14 @@ const postIDs = async () => {
 
 const postRowEventHandler = async () => {
     let totalPosts = await postIDs();
+    const postRows = document.querySelectorAll('.postrow');
     let rowID = [];
 
-    for (let j = 0; j < totalPosts.length; j++) {
+    for (let j = 0; j < postRows.length; j++) {
         rowID.push(postRows[j].getAttribute('id'));
     }
 
-    for (let i = 0; i < totalPosts.length; i++) {
+    for (let i = 0; i < postRows.length; i++) {
         postRows[i].addEventListener('click', async (event) => {
             event.preventDefault();
             document.location.replace(`/posts/${rowID[i]}`);
@@ -24,5 +22,3 @@ const postRowEventHandler = async () => {
 }
 
 postRowEventHandler();
-
-
