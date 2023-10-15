ce6c1ad1e9371344a5c502137184d0e2eda94fb3
jeremysr <jeremysr@protonmail.com>
2023-10-14 20:36:54 -0600

    adding deploy connections for railway

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,15 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 //this will use the environment variables stored in the .env file to log into the msql database
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
+const sequelize = process.env.MYSQL_URL
+  ? new Sequelize(process.env.MYSQL_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
       },
+      port:3306,
     });
 
 module.exports = sequelize;
